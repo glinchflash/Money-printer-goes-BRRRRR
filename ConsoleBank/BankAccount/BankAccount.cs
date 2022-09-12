38dbfe0d59a469281685638078092e6d6ba78763
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-09-12 17:11:41 +0200

    Almost done with exercise, can make new bank account for a new client, Can check balance, withdraw and deposit.
    TODO: fix loop issue to end actions with bank.

--- ConsoleBank/BankAccount/BankAccount.cs
+++ ConsoleBank/BankAccount/BankAccount.cs
@@ -3,35 +3,63 @@ using ClientManagement;
 
 public class BankAccount
 {
-    public Client _client;
-
-    //private Type type;
-
-    public int Balance = 0;
-    {
-        get { return Balance; }
-        set { Balance = value; }
+    public enum Type {
+        CheckingsAccount,
+        SavingsAccount,
     }
     
+    public Client _client;
+    public Type type;
+    public int Balance = 0;
     
-    public BankAccount(int initDeposit, Client client)
+    public BankAccount(int initDeposit, Client client, int typeChoose)
     {
         this.Balance += initDeposit;
         this._client = client;
+        this.type = TypeSelection(typeChoose);
     }
-    /*
-    public int CheckBalance
+    
+    public int CheckBalance()
     {
-        //
-    }
-
-    public int Withdraw
-    {
-        //
+        return Balance;
     }
 
-    public int Deposit
+    public int Withdraw(int value)
     {
-        //
-    }*/
+        if (value <= 0)
+        {
+            throw new Exception("Amount has to be bigger then 0");
+        }
+        
+        if (this.Balance - value < 0)
+        {
+            throw new Exception("Insufficient balance to withdraw this amount!");
+        }
+        return this.Balance -= value;
+    }
+
+    public int Deposit(int value)
+    {
+        if (value <= 0)
+        {
+            throw new Exception("Amount has to be bigger then 0");
+        }
+        return this.Balance += value;
+    }
+
+    public  Type TypeSelection(int Selector)
+    {
+        if (Selector == 1)
+        {
+           return type = Type.CheckingsAccount;
+        }
+        else if (Selector == 2)
+        {
+            return type = Type.SavingsAccount;
+        }
+        else
+        {
+            return type = Type.CheckingsAccount;
+        }
+    }
 }
--- ConsoleBank/BankAccount/Program.cs
+++ ConsoleBank/BankAccount/Program.cs
@@ -4,6 +4,9 @@ using ClientManagement;
 //variables
 string? name = "";
 string? initialBalance="";
+string? typeselection = "";
+string? actionSelection = "1";
+bool quit = false;
 //asking for owners name and defining it
 Console.WriteLine("Hey, what is the name of the owner of this your new bank account?");
 if (string.IsNullOrEmpty(name))
@@ -15,7 +18,6 @@ if (name is not null)
 {
     var currentDate = DateTime.Now;
     Client client = new Client(1, name, currentDate);
-    Console.WriteLine(client.name);
     // asking for initial balance/deposit and defining it in the bankaccount
     Console.WriteLine("And how much would you like to start with as your initial balance? (first deposit)");
     if (string.IsNullOrEmpty(initialBalance))
@@ -24,14 +26,80 @@ if (name is not null)
         
     }
 
-    if (initialBalance is not null && initialBalance is string)
+    if (string.IsNullOrEmpty(typeselection))
     {
-        Console.WriteLine(int.Parse(initialBalance));
+        Console.WriteLine("What kind of account do you want to open?");
+        Console.WriteLine("Select 1 for a checkings account");
+        Console.WriteLine("Select 2 for a savings account");
+        typeselection = Console.ReadLine();
+    }
+
+
+    if (initialBalance != null && typeselection != null)
+    {
         //creating new account
-        BankAccount bankAccount = new BankAccount(int.Parse(initialBalance), client);
-        Console.WriteLine(bankAccount.Balance + " is your new balance and is created for " +client.name);
+        BankAccount bankAccount = new BankAccount(int.Parse(initialBalance), client, int.Parse(typeselection));
+        Console.WriteLine("Your new " + bankAccount.type + " has been opened on following name: ");
+        Console.WriteLine(client.name);
+        Console.WriteLine("And your current balance is: " + bankAccount.Balance);
+    
+        if (quit != true)
+        { 
+            Console.WriteLine("Select what you wanna do:");
+            Console.WriteLine("Select 1 for checking your balance");
+            Console.WriteLine("Select 2 for withdrawing from your account");
+            Console.WriteLine("Select 3 for depositing on your account");
+            Console.WriteLine("Select anything else to stop!");
+            if (string.IsNullOrEmpty(actionSelection))
+            {
+                actionSelection = Console.ReadLine();
+            }
+
+            if (actionSelection == "1")
+            {
+                Console.WriteLine(bankAccount.CheckBalance());
+            }
+            else if (actionSelection == "2")
+            {
+                Console.WriteLine("How much do you want to withdraw?");
+                string? WithdrawAmount = Console.ReadLine();
+                if (WithdrawAmount != null)
+                {
+                    bankAccount.Withdraw(int.Parse(WithdrawAmount));
+                    Console.WriteLine("You withdrawed " + WithdrawAmount + " of your account");
+                    Console.WriteLine("Current balance: " + bankAccount.CheckBalance());
+                }
+            }
+            else if (actionSelection == "3")
+            {
+                Console.WriteLine("How much do you wanna deposit?");
+                string? DepositAmount = Console.ReadLine();
+                if (DepositAmount != null)
+                {
+                    try
+                    {
+                        bankAccount.Deposit(int.Parse(DepositAmount));
+                        Console.WriteLine("You deposited " + DepositAmount + " to your account");
+                        Console.WriteLine("Your current balance is now: " + bankAccount.CheckBalance());
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        throw;
+                    }
+                }
+            }
+            else if (actionSelection != "1" && actionSelection != "2" && actionSelection != "3")
+            {
+                Console.WriteLine("Thank you for your bussiness, Untill the next time!");
+                quit = true;
+                
+            }
+           
+        }
     }
 }
+
 
 
 
