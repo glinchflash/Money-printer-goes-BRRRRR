a45fe849b116080e55939fc46e53089b77a2c260
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-09-13 08:57:57 +0200

    Fixed loop issue! at the moment you start with giving your name on who's behalf the account should be opened, your initial deposit, what kind of account it is, and the option to check balance, withdraw and deposit and after your selection you can select again if you wanna do something else. Or stop by inputting anything else then the choice selectors.

--- ConsoleBank/BankAccount/Program.cs
+++ ConsoleBank/BankAccount/Program.cs
@@ -5,8 +5,7 @@ using ClientManagement;
 string? name = "";
 string? initialBalance="";
 string? typeselection = "";
-string? actionSelection = "1";
-bool quit = false;
+string? actionSelection = "";
 //asking for owners name and defining it
 Console.WriteLine("Hey, what is the name of the owner of this your new bank account?");
 if (string.IsNullOrEmpty(name))
@@ -42,22 +41,22 @@ if (name is not null)
         Console.WriteLine("Your new " + bankAccount.type + " has been opened on following name: ");
         Console.WriteLine(client.name);
         Console.WriteLine("And your current balance is: " + bankAccount.Balance);
-    
-        if (quit != true)
-        { 
-            Console.WriteLine("Select what you wanna do:");
-            Console.WriteLine("Select 1 for checking your balance");
-            Console.WriteLine("Select 2 for withdrawing from your account");
-            Console.WriteLine("Select 3 for depositing on your account");
-            Console.WriteLine("Select anything else to stop!");
-            if (string.IsNullOrEmpty(actionSelection))
-            {
+        
+        
+        while (string.IsNullOrEmpty(actionSelection))
+        {
+                Console.WriteLine("Select what you wanna do:");
+                Console.WriteLine("Select 1 for checking your balance");
+                Console.WriteLine("Select 2 for withdrawing from your account");
+                Console.WriteLine("Select 3 for depositing on your account");
+                Console.WriteLine("Select anything else to stop!");
                 actionSelection = Console.ReadLine();
-            }
+            
 
             if (actionSelection == "1")
-            {
-                Console.WriteLine(bankAccount.CheckBalance());
+            { 
+                Console.WriteLine("Your current balance is : "+bankAccount.CheckBalance());
+                actionSelection = "";
             }
             else if (actionSelection == "2")
             {
@@ -69,6 +68,7 @@ if (name is not null)
                     Console.WriteLine("You withdrawed " + WithdrawAmount + " of your account");
                     Console.WriteLine("Current balance: " + bankAccount.CheckBalance());
                 }
+                actionSelection = "";
             }
             else if (actionSelection == "3")
             {
@@ -81,6 +81,7 @@ if (name is not null)
                         bankAccount.Deposit(int.Parse(DepositAmount));
                         Console.WriteLine("You deposited " + DepositAmount + " to your account");
                         Console.WriteLine("Your current balance is now: " + bankAccount.CheckBalance());
+                        actionSelection = "";
                     }
                     catch (Exception e)
                     {
@@ -89,16 +90,15 @@ if (name is not null)
                     }
                 }
             }
-            else if (actionSelection != "1" && actionSelection != "2" && actionSelection != "3")
+            else
             {
                 Console.WriteLine("Thank you for your bussiness, Untill the next time!");
-                quit = true;
-                
+                break;
             }
-           
         }
     }
 }
+
 
 
 
